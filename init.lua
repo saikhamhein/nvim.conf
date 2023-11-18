c34e8187967c81082447130c3825bdf0402b55ad
Sai Kham Hein <saikhamhein@gmail.com>
2023-11-18 23:43:06 +0800

    add telescope, shows space as dot

--- init.lua
+++ init.lua
@@ -11,7 +11,23 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- remapping
 vim.g.mapleader = " "
 
+-- settings
+vim.wo.relativenumber = true
+vim.wo.number = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.opt.list = true
+vim.opt.listchars = "tab:🡢 ,trail:·,extends:>,precedes:<,space:·"
 require("lazy").setup("plugins")
 vim.cmd.colorscheme "catppuccin"
+
+--telescope bindings
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
