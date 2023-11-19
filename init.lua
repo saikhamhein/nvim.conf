8b40235b6f8250411fd19382ef57099d6720739f
Sai Kham Hein <saikhamhein@gmail.com>
2023-11-19 16:57:13 +0800

    add telescope-fzf-native.nvim

--- init.lua
+++ init.lua
@@ -31,3 +31,5 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
+--telscope extensions
+require('telescope').load_extension('fzf')
